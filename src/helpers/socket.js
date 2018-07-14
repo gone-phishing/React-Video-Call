e3acdfd2e4bb92a1061aee17e3be009dc647e0f7
Ritesh Kumar Singh <riteshoneinamillion@gmail.com>
2018-07-15 01:23:16 +0530

    COnverting htttp to https

--- src/helpers/socket.js
+++ src/helpers/socket.js
@@ -5,7 +5,7 @@ if(window.location.origin.indexOf("localhost") > 0) {
 	socket = io('http://localhost:5000');	
 }
 else {
-	socket = io('http://api.flinkseed.com');
+	socket = io('https://api.flinkseed.com');
 }
 
 export default socket;
